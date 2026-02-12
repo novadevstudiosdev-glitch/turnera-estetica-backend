b5857160a56e508c5983ebdf6488e753be4d3c92
leon_demand <leopaz@unicauca.edu.co>
2026-02-12 21:34:45 +0100

    chore(project): instalar dependencias e inicializar estructura de carpetas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServicesModule } from './modules/services/services.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ServicesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
